06068767f56be4edea0da3ab7be2a9603d8f016a
yash kohli <yashkohli786@gmail.com>
2020-08-04 01:08:54 +0530

    ui completed,major texting and matching left

--- src/Chat.js
+++ src/Chat.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./chat.css";
 import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
 function Chat({ name, message, profilePic, timestamp }) {
   return (
-    <div className="chat">
-      <Avatar className="chat__image" src={profilePic} />
-      <div className="chat__details">
-        <h2>{name}</h2>
-        <p>{message}</p>
+    <Link to={`/chat/${name}`}>
+      <div className="chat">
+        <Avatar className="chat__image" src={profilePic} />
+        <div className="chat__details">
+          <h2>{name}</h2>
+          <p>{message}</p>
+        </div>
+        <p className="chat__timestamp">{timestamp}</p>
       </div>
-      <p className="chat__timestamp">{timestamp}</p>
-    </div>
+    </Link>
   );
 }
 
--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-
+import "./ChatScreen.css";
+import { Avatar } from "@material-ui/core";
 function ChatScreen() {
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([
     {
       name: "Halsey",
@@ -15,17 +17,52 @@ function ChatScreen() {
       message: "yo yo whats up",
     },
     {
-      message: "hiiii",
+      message: "hiiii,how are you??",
     },
   ]);
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { message: input }]);
+    setInput("");
+  };
   return (
     <div className="chatScreen">
-      <p>You Matched with halsey on 10/10/20</p>
-      {messages.map((message) => (
-        <div>
-          <p>{message.message}</p>
-        </div>
-      ))}
+      <p className="chatScreen__timestamp">
+        You Matched with halsey on 10/10/20
+      </p>
+      {messages.map((message) =>
+        message.name ? (
+          <div className="chatScreen__message">
+            <Avatar
+              className="chatScreen__image__user"
+              alt={message.name}
+              src={message.image}
+            />
+            <p className="chatScreen__text">{message.message}</p>
+          </div>
+        ) : (
+          <div className="chatScreen__message">
+            <p className="chatScreen__textUser">{message.message}</p>
+          </div>
+        )
+      )}
+      <form className="chatScreen__input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="chatScreen__inputField"
+          type="text"
+          placeholder="Type a message..."
+        />
+        <button
+          onClick={handleSend}
+          type="submit"
+          className="chatScreen__inputButton"
+        >
+          SEND
+        </button>
+      </form>
     </div>
   );
 }
